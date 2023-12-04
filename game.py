060155fc173fe4bcb0aca84ac72d48f35c4ed39e
parsaprogrammer2 <ParsaPR66@gmail.com>
2023-12-04 12:29:14 +0330

    Hangman

---
+++ game.py
@@ -0,0 +1,38 @@
+from random import *
+from sys import *
+for s in range(3):
+    team=randint(1,12)
+    team=randint(1,12)
+    team1="real madrid"
+    team2="man city"
+    team3="bayern"
+    team4="milan"
+    team5="man younaited"
+    team6="arsenal"
+    team7="barcelona"
+    team8="psg"
+    team9="inter"
+    team10="dortmond"
+    team11="porto"
+    team12="persepolis"
+    goal=randint(1,12)
+    goal1=randint(1,12)
+    goal2=randint(1,12)
+    goal3=randint(1,12)
+    goal4=randint(1,12)
+    goal5=randint(1,12)
+    goal6=randint(1,12)
+    goal7=randint(1,12)
+    goal8=randint(1,12)
+    goal9=randint(1,12)
+    goal10=randint(1,12)
+    goal11=randint(1,12)
+    goal12=randint(1,12)
+    if team==team:
+        break
+        exit()
+    if goal==goal:
+        break
+        exit()
+dog_name1=100
+print(dog_name1)
